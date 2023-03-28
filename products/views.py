49c4414e2afd311997ce84418af43f37f712a708
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-03-28 12:26:09 +0300

    update urls for products, create model users

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from products.models import Product, ProductCategory
 
+
 def index(request):
     context = {'title': 'Store'}
     return render(request, 'products/index.html', context)
