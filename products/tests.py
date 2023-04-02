e71e1b8f7e61825b528c4554fd4180b0d7e911e0
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-04-02 09:03:42 +0300

    added tests for ProductsListView and fixed bugs in products.html

--- products/tests.py
+++ products/tests.py
@@ -3,8 +3,10 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from products.models import Product, ProductCategory
 
-class IndexViewTEstCase(TestCase):
+
+class IndexViewTestCase(TestCase):
 
     def test_view(self):
         path = reverse('index')
@@ -13,3 +15,33 @@ class IndexViewTEstCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Store')
         self.assertTemplateUsed(response, 'products/index.html')
+
+
+class ProductsListViewTestCase(TestCase):
+    fixtures = ['categories.json', 'goods.json']
+
+    def setUp(self) -> None:
+        self.products = Product.objects.all()
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        self._common_test(response)
+        self.assertEqual(list(response.context_data['object_list']), list(self.products[:3]))
+
+    def test_list_category(self):
+        category = ProductCategory.objects.first()
+        path = reverse('products:catinator', kwargs={'category_id': category.id, 'page': 1})
+        response = self.client.get(path)
+
+        self._common_test(response)
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.products.filter(category_id=category.id)[:3])
+        )
+
+    def _common_test(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Каталог')
+        self.assertTemplateUsed(response, 'products/products.html')
