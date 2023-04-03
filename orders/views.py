7bd214df69c18b7a72bcc961c46ede34d4e6cfab
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-04-03 23:16:27 +0300

    added viewing a specific order in the list of orders

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,14 @@
-import stripe
 from http import HTTPStatus
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse_lazy, reverse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic.edit import CreateView
-from django.views.generic.base import TemplateView
-from django.views.generic.list import ListView
+
+import stripe
 from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
@@ -34,6 +36,16 @@ class OrderListView(TitleMixin, ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(initiator=self.request.user)
+
+
+class OrderDetailView(DetailView):
+    template_name = 'orders/order.html'
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f"Store - Заказ №{self.object.id}"
+        return context
 
 
 class OrderCreateView(TitleMixin, CreateView):
