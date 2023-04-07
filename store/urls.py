466cccf241b37cb03ed6efa598e0f5c4fdb7608c
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-04-07 21:44:18 +0300

    added permissions for api

--- store/urls.py
+++ store/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from orders.views import stripe_webhook_view
 from products.views import IndexView
@@ -29,7 +30,8 @@ urlpatterns = [
     path('orders/', include('orders.urls', namespace='orders')),
     path('webhook/stripe/', stripe_webhook_view, name='stripe_webhook'),
     path('accounts/', include('allauth.urls')),
-    path('api/', include('api.urls', namespace='api'))
+    path('api/', include('api.urls', namespace='api')),
+    path('api-token-auth/', obtain_auth_token)
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
